5643ae513b00f95568cd6f1c23991d13195e4936
khshakilahamed <khshakil.ahamed18@gmail.com>
2021-12-01 12:05:33 +0600

    used fake data with projects

--- fakeData.js
+++ fakeData.js
@@ -1,44 +1,82 @@
+// autox-
+// https://i.ibb.co/2FCq3qn/auto-X-short.jpg
+// https://i.ibb.co/LnfHs1t/autox-1.png
+// https://i.ibb.co/H4ZWcxF/autox-2.png
+// https://i.ibb.co/smhH4MG/autox-3.png
+
+// world tour-
+// https://i.ibb.co/3rtjLvZ/world-tour-short.jpg
+// https://i.ibb.co/z5z18m7/world-tour-1.png
+// https://i.ibb.co/M5jzN9P/world-tour-2.png
+// https://i.ibb.co/Pj4TZfL/world-tour-3.png
+
+// silver gym-
+// https://i.ibb.co/GMbtGnp/silver-gym-short.jpg
+// https://i.ibb.co/vkJbrNy/silver-gym-1.png
+// https://i.ibb.co/k6S9ngL/silver-gym-2.png
+// https://i.ibb.co/1Lgq7Gh/silver-gym-3.png
+
 const data = [
     {
         id: 1,
         projectName: "autoX- The Online Bike Store",
-        shortImg: '',
+        shortImg: 'https://i.ibb.co/2FCq3qn/auto-X-short.jpg',
         technologies: 'CSS, React, Node.js, Express, MongoDB, Bootstrap5, Heroku, Firebase,Authentication, Firebase Deploy.',
         liveSite: 'https://auto-x-work-12.web.app/',
         clientCode: 'https://github.com/khshakilahamed/auto-x-client-site-work-12',
         serverCode: 'https://github.com/khshakilahamed/auto-x-server-site-work-12',
         imgDetails1: {
-            img: '',
+            img: 'https://i.ibb.co/LnfHs1t/autox-1.png',
             details: ['This is the home page. Where a user can see the Navbar., Banner, Offered Brands, Bikes, Customer Reviews, and the Footer.', 'From the home page, a user can order a bike or can see the all bikes to click the Explore button on the navbar.', 'A user also can goes the Dashboard.', 'When a user goes to the Dashboard, then the user can see the My Orders, Make Payment, Review, and Logout.', 'Similarly, When an admin goes to the Dashboard, then the admin can see some nested routes like- Manage Orders, Manage Products, Add a product, Make Admin, and Logout.']
         },
         imgDetails2: {
-            img: '',
+            img: 'https://i.ibb.co/H4ZWcxF/autox-2.png',
             details: ['When a user wants to see all bikes, then the user easily can see the all bike to click on the explore.', 'Here, the user can find the search bar, through the search bar the user can search a bike', 'Also a user orders a bike from here.']
         },
         imgDetails3: {
-            img: '',
+            img: 'https://i.ibb.co/smhH4MG/autox-3.png',
             details: ['This is the Dashboard of an admin. A normal user can not enter here.', 'When an admin login, then the admin will be about to find out some nested out like- Manage Orders, Manage Products, Add a Product, Make Admin.', 'Manage orders- Through this route, the user can see the orders of all users and the admin can delete the order or can approve the order.']
         }
     },
     {
         id: 2,
         projectName: "WorldTourBD- The Travel Agency",
-        shortImg: '',
+        shortImg: 'https://i.ibb.co/3rtjLvZ/world-tour-short.jpg',
         technologies: 'CSS, React, Node.js, Express, MongoDB, Bootstrap5, Heroku, Firebase,Authentication, Firebase Deploy.',
         liveSite: 'https://world-tour-bd-work-11.web.app/',
         clientCode: 'https://github.com/khshakilahamed/world-tour-bd-work-11',
         serverCode: 'https://github.com/khshakilahamed/world-tour-bd-work-11-server',
         imgDetails1: {
-            img: '',
+            img: 'https://i.ibb.co/z5z18m7/world-tour-1.png',
             details: ['This is the home page or landing page of this website', 'A user can see the Home and About on the navbar without login. A can be login when the user clicks on login button which is on the navbar.', 'When a user will be logged in, then the user can find some more routes like- My Orders, Manage All Orders, All New Service', 'On this page, the user can see the Services,']
         },
         imgDetails2: {
-            img: '',
+            img: 'https://i.ibb.co/M5jzN9P/world-tour-2.png',
             details: ['When a user wants to see details of a service, then the user can click on the see more button and goes to another page.', 'When the user arrived at the details page then the user can see the big picture, package tile, price of the package, description, and the contact section.']
         },
         imgDetails3: {
-            img: '',
+            img: 'https://i.ibb.co/Pj4TZfL/world-tour-3.png',
             details: ['The About page-', 'The user can see the short detail of the agency', 'The agency company commitment etc.'],
+        }
+    },
+    {
+        id: 3,
+        projectName: "Silver GYM- The Gym Website",
+        shortImg: 'https://i.ibb.co/GMbtGnp/silver-gym-short.jpg',
+        technologies: 'React, CSS, Bootstrap5, Firebase Authentication, Firebase Deploy.',
+        liveSite: 'https://silver-bd-gym-assignment-10.web.app/',
+        clientCode: 'https://github.com/khshakilahamed/silver-bd-gym-work-10',
+        imgDetails1: {
+            img: 'https://i.ibb.co/vkJbrNy/silver-gym-1.png',
+            details: ['First of all, when a user visits the website can see some major sections like- Benefits of Gym, Services, Packages, Trainers', 'If a user wants to see more details/ more services/ more package without login then the user will reach the login section. Cause these sections are private. The user needs to login then the user goes to those sections.']
+        },
+        imgDetails2: {
+            img: 'https://i.ibb.co/k6S9ngL/silver-gym-2.png',
+            details: ['The login page- The user can easily register here with email and password then easily can log in the website.', 'On the other hand, the can login with Google, Facebook and GitHub', 'After logging in, the user can see the logout button on the right side of the navbar. To click this button user gets to log out from the website.']
+        },
+        imgDetails3: {
+            img: 'https://i.ibb.co/1Lgq7Gh/silver-gym-3.png',
+            details: ['Now, the Blog page.', 'Here are some blogs about our health. To read these blogs, humans could be careful about their health and how could be healthy.'],
         }
     },
 ]
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import autoX from '../../images/projects/autoX.jpg';
 import worldTour from '../../images/projects/world_tour.jpg';
 import silverGym from '../../images/projects/silver_gym.jpg';
@@ -6,6 +6,15 @@ import { Fade } from 'react-reveal';
 import './Projects.css'
 
 const Projects = () => {
+    const [projects, setProjects] = useState([]);
+    useEffect(() => {
+        fetch('/fakeData.json')
+            .then(res => res.json())
+            .then(data => setProjects(data))
+    }, []);
+
+    console.log(projects);
+
     return (
         <div className="container" id="projects" style={{ height: '80vh' }}>
             <div className="my-5 py-4">
@@ -15,23 +24,25 @@ const Projects = () => {
             <Fade bottom>
                 <section className="projects-container">
                     <div className="row">
-                        <div className="col-md-4 text-center ">
-                            <div className="p-3">
-                                <div className="project-container">
-                                    <img className="img-fluid w-100 project-img" src={autoX} alt="" />
-                                    <div className="project-content d-flex align-items-center justify-content-center">
-                                        <div>
-                                            <h4>AutoX- The Bike Online Store</h4>
-                                            <a href="https://auto-x-work-12.web.app/" target="_blank"><i className="fas fa-external-link-alt fa-2x m-4"></i></a>
-                                            <a href="https://github.com/khshakilahamed/auto-x-client-site-work-12" target="_blank"><i class="fab fa-github fa-2x m-4"></i></a>
-                                            <a href="https://github.com/khshakilahamed/auto-x-server-site-work-12" target="_blank"><i class="fas fa-server fa-2x m-4"></i></a>
-                                            <p><a href="#">View Details</a></p>
+                        {
+                            projects.map(project => <div key={project.id} className="col-md-4 text-center ">
+                                <div className="p-3">
+                                    <div className="project-container">
+                                        <img className="img-fluid w-100 project-img" src={project.shortImg} alt="" />
+                                        <div className="project-content d-flex align-items-center justify-content-center">
+                                            <div>
+                                                <h4>{project.projectName}</h4>
+                                                <a href={project.liveSite} target="_blank"><i className="fas fa-external-link-alt fa-2x m-4"></i></a>
+                                                <a href={project.clientCode} target="_blank"><i class="fab fa-github fa-2x m-4"></i></a>
+                                                <a href={project.serverCode} target="_blank"><i class="fas fa-server fa-2x m-4"></i></a>
+                                                <p><a href="#">View Details</a></p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 text-center ">
+                            </div>)
+                        }
+                        {/* <div className="col-md-4 text-center ">
                             <div className="p-3">
                                 <div className="project-container">
                                     <img className="img-fluid w-100 project-img" src={worldTour} alt="" />
@@ -61,7 +72,7 @@ const Projects = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
 
                     </div>
                 </section>
